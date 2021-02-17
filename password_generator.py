963dbd6df66715f7d3da2b33db4d09d726d8a640
tym21 <56368000+timknothe@users.noreply.github.com>
2021-02-17 23:29:31 +0100

    all code

---
+++ password_generator.py
@@ -0,0 +1,20 @@
+import secrets
+import string
+
+# all characters for the password
+chars = string.digits + string.ascii_letters + string.punctuation
+
+
+# ask the user how many characters the password should have
+def get_character_count():
+    count: str = input("How many characters should the password have: ")
+    try:
+        return int(count)
+    except ValueError:
+        print("The input must be a number.")
+        get_character_count()
+
+
+print("- Password Generator by tym21 -\n")
+length = get_character_count()
+print("".join(secrets.choice(chars) for _ in range(length)))
